258438436f807812615ef7ad0894604014e7a081
Stephen More <stephen.more@gmail.com>
2019-03-28 07:46:35 -0400

    Configuring as much as possible.

--- oidc-azure/src/app/app.component.ts
+++ oidc-azure/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { OAuthService, JwksValidationHandler, AuthConfig } from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+issuer: 'https://login.microsoftonline.com/<enter guid here>/v2.0',
+  redirectUri: window.location.origin,
+  clientId: '<enter client id here>'
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oidc-azure';
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    this.oauthService.strictDiscoveryDocumentValidation = false;
+    this.oauthService.responseType = 'code token id_token';
+    this.oauthService.scope = 'openid';
+
+    /* This needs to be set manually b/c ADFS and Azure AD do not support CORS */
+
+    /*
+    * From this url: https://login.microsoftonline.com/<enter domain name>/v2.0/.well-known/openid-configuration
+    * you can find the loginUrl and tokenEndpoint
+     */
+
+    this.oauthService.userinfoEndpoint = 'https://graph.microsoft.com/oidc/userinfo';
+    this.oauthService.loginUrl = 'https://login.microsoftonline.com/<enter guid here>/oauth2/v2.0/authorize';
+    this.oauthService.tokenEndpoint = 'https://login.microsoftonline.com/<enter guid here>/oauth2/v2.0/token/';
+  }
+
+  login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  get givenName() {
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) {
+      return null;
+    }
+    return claims['name'];
+  }
 }
--- oidc-azure/src/app/app.module.ts
+++ oidc-azure/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
